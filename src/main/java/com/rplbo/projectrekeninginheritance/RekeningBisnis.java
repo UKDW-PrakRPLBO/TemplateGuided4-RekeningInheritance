fad432dd2c00c83d46f8863806c6493706286b97
Antonius Rachmat <antoniusrc@users.noreply.github.com>
2025-02-22 18:38:27 +0700

    Update RekeningBisnis.java

--- src/main/java/com/rplbo/projectrekeninginheritance/RekeningBisnis.java
+++ src/main/java/com/rplbo/projectrekeninginheritance/RekeningBisnis.java
@@ -2,25 +2,20 @@ package com.rplbo.projectrekeninginheritance;
 
 public class RekeningBisnis extends Rekening {
 
-    public final double BUNGA_SETOR = 0.001;
-
     public RekeningBisnis(Nasabah nasabah) {
-        super(nasabah);
+        
     }
 
     public RekeningBisnis(Nasabah nasabah, int saldo) {
-        super(nasabah, saldo);
+        
     }
 
     public void penyetoran(int jumlah) {
-        if (jumlah > 0) {
-            double totalSetoran = jumlah + bunga(jumlah);
-            super.penyetoran((int) totalSetoran);
-        }
+
     }
 
     private double bunga(int jumlah) {
-        return BUNGA_SETOR * jumlah;
+        
     }
 }
 
